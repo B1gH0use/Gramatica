fe53938571bf6418f30b5881bde3f9ef1eabd4da
Victor Farias Casagrande <victorcasag@hotmail.com>
2020-04-18 23:02:12 -0300

    Now i can take the information from Jtable

--- ClassGUILibrary/src/Package/libraryPilha.java
+++ ClassGUILibrary/src/Package/libraryPilha.java
@@ -9,33 +9,79 @@ public class libraryPilha {
 	private static Stack<String> pilha = new Stack<String>();
 	private static Stack<String> saida = new Stack<String>();
 	
-	public static void nonTerminalClass(String posicao,String sentenca) {
-		if (validations(sentenca) == false) return;
+	public static void nonTerminalClass(/*String posicao,*/Object sentenca) {
+		//if (validations(sentenca.toString()) == false) return;
 		try {
-			sentenca = sentenca.trim().replaceAll(" ", "");
-			String[] sentencaSplit = sentenca.split("->");
-			sentencaSplit = sentenca.split("|");
+			//sentenca = sentenca.replaceAll(" ", "");
+			//String[] sentencaSplit = {"teste"}; //sentenca.toString().split("->");
+			//sentencaSplit = sentenca.toString().split("|");
 			
-			for(int i = sentencaSplit.length-1 ; i>=0; i--) {
+			String se = sentenca.toString();
+			
+			if(se.startsWith("S")) {
+				se = se.replaceAll(" ", "");
+				String[] sentencaSplit = se.split("->");
+				sentencaSplit = se.split("|");
+				
+				for(int i = se.length()-1; i >= 0 ; i--) {
+					String t = sentencaSplit[i];
+					pilha.push(t);
+					if (t.contains("-") || (t.contains(">"))) {
+						pilha.pop();
+					}
+					if(pilha.peek().toUpperCase() == t) {
+						if(!pilha.isEmpty()) {
+							pilha.pop();
+							if(t.toLowerCase() == t) {
+								saida.push(t);
+							}else {
+								
+							}
+			        	}
+					}else {
+						JOptionPane.showMessageDialog(null, "Nao Terminal Invalido");
+						return;
+					}
+				}	
+				
+				while(!pilha.empty()) {
+		            System.out.println("[" + pilha.pop() + "]");
+		        }
+			}
+			
+			
+			/*for(int i = 0; i<sentenca.; i++) {
 				String t = sentencaSplit[i];
 				pilha.push(t);
 				if (t.contains("-") || (t.contains(">"))) {
 					pilha.pop();
 				}	
-				if (pilha.peek().equals("S")) {
-					if(!pilha.isEmpty()) {
-						pilha.pop();
-						if(t.toLowerCase() == t) {
-							saida.push(t);
-						}
-		        	}
+				while(!pilha.empty()) {
+		           // System.out.println("[" + pilha.pop() + "]");
+		        }
+				if(pilha.peek().toUpperCase() == t) {
+					if (pilha.peek().equals("S")) {
+						if(!pilha.isEmpty()) {
+							pilha.pop();
+							if(t.toLowerCase() == t) {
+								saida.push(t);
+							}else {
+								
+							}
+								
+			        	}
+					}else {
+						while(!pilha.empty()) {
+				            System.out.println("[" + pilha.pop() + "]");
+				        }
+						return;
+					}
 				}else {
-					while(!pilha.empty()) {
-			            System.out.println("[" + pilha.pop() + "]");
-			        }
+					JOptionPane.showMessageDialog(null, "Começo Invalido");
 					return;
 				}
-			}
+			}*/
+			
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Erro:" + e.getMessage());
 		}
@@ -43,10 +89,10 @@ public class libraryPilha {
 	}
 	
 	
-	public static boolean validations(String sentenca) {
+	public static boolean validations(Object sentenca) {
 		boolean ok = false;
 		try {
-			if (!sentenca.contains("->")) {
+			if (!sentenca.toString().contains("->")) {
 				throw new Exception("Formato valido: X->Xx|Xx");
 			}	
 			return ok;
